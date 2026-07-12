940ca606c2fecc8b9725a7986af046b4a6588453
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:05 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "security_center_assessments" {
-  description = "All security_center_assessment resources"
-  value       = azurerm_security_center_assessment.security_center_assessments
-}
 output "security_center_assessments_additional_data" {
-  description = "List of additional_data values across all security_center_assessments"
-  value       = [for k, v in azurerm_security_center_assessment.security_center_assessments : v.additional_data]
+  description = "Map of additional_data values across all security_center_assessments, keyed the same as var.security_center_assessments"
+  value       = { for k, v in azurerm_security_center_assessment.security_center_assessments : k => v.additional_data }
 }
 output "security_center_assessments_assessment_policy_id" {
-  description = "List of assessment_policy_id values across all security_center_assessments"
-  value       = [for k, v in azurerm_security_center_assessment.security_center_assessments : v.assessment_policy_id]
+  description = "Map of assessment_policy_id values across all security_center_assessments, keyed the same as var.security_center_assessments"
+  value       = { for k, v in azurerm_security_center_assessment.security_center_assessments : k => v.assessment_policy_id }
 }
 output "security_center_assessments_status" {
-  description = "List of status values across all security_center_assessments"
-  value       = [for k, v in azurerm_security_center_assessment.security_center_assessments : v.status]
+  description = "Map of status values across all security_center_assessments, keyed the same as var.security_center_assessments"
+  value       = { for k, v in azurerm_security_center_assessment.security_center_assessments : k => v.status }
 }
 output "security_center_assessments_target_resource_id" {
-  description = "List of target_resource_id values across all security_center_assessments"
-  value       = [for k, v in azurerm_security_center_assessment.security_center_assessments : v.target_resource_id]
+  description = "Map of target_resource_id values across all security_center_assessments, keyed the same as var.security_center_assessments"
+  value       = { for k, v in azurerm_security_center_assessment.security_center_assessments : k => v.target_resource_id }
 }
 
